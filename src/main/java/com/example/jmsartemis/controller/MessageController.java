dc48b14a974cf046b96e1cbc943c978b3f89a986
TeaEye <ilya.tomskih@gmail.com>
2022-05-03 15:09:28 +0700

    Initial commit

---
+++ src/main/java/com/example/jmsartemis/controller/MessageController.java
@@ -0,0 +1,21 @@
+package com.example.jmsartemis.controller;
+
+import com.example.jmsartemis.service.DispatcherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+    @Autowired
+    DispatcherService dispatcherService;
+
+    @PostMapping(value = "/send")
+    public ResponseEntity<String> send(@RequestBody String message) {
+        dispatcherService.sendMessage(message);
+        return new ResponseEntity<>("Message [" + message + "] has been sent", HttpStatus.OK);
+    }
+}
---
+++ src/main/java/com/example/jmsartemis/service/ReceiverService.java
@@ -0,0 +1,16 @@
+package com.example.jmsartemis.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReceiverService {
+    Logger logger = LoggerFactory.getLogger(ReceiverService.class);
+
+    @JmsListener(destination = "${jms.queue}")
+    public void receiveMessage(String message) {
+        logger.info("Received: " + message);
+    }
+}
