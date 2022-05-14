86d0cf276dd605e2aa29b9a8d600d65d9521c3e6
TeaEye <ilya.tomskih@gmail.com>
2022-05-14 15:43:10 +0700

    .

--- src/main/java/com/example/jmsartemis/service/ReceiverService.java
+++ src/main/java/com/example/jmsartemis/service/ReceiverService.java
@@ -5,12 +5,16 @@ import org.slf4j.LoggerFactory;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
 
+
 @Service
 public class ReceiverService {
     Logger logger = LoggerFactory.getLogger(ReceiverService.class);
 
     @JmsListener(destination = "${jms.queue}")
     public void receiveMessage(String message) {
-        logger.info("Received: " + message + ", free memory in heap: " + Runtime.getRuntime().freeMemory() + " bytes.");
+        logger.info("\nThread ID = " + Thread.currentThread().getId() +
+                    "\nReceived: " + message +
+                    "\nFree memory in heap: " + Runtime.getRuntime().freeMemory() + " bytes." +
+                    "\n________________________________________________________________________\n");
     }
 }
