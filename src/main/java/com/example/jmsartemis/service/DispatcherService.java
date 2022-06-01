ff1cb3fd884e0b56b24f6bfcfa4aa93f05bb2f74
TeaEye <ilya.tomskih@gmail.com>
2022-06-02 00:22:56 +0700

    .

--- src/main/java/com/example/jmsartemis/service/DispatcherService.java
+++ src/main/java/com/example/jmsartemis/service/DispatcherService.java
@@ -1,6 +1,5 @@
 package com.example.jmsartemis.service;
 
-import com.example.jmsartemis.controller.MessageController;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
